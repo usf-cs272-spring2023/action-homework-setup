5870e49585acc512ecd887e5db100ef05786010e
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-26 19:42:07 -0800

    Adding debug output

--- check_warnings.sh
+++ check_warnings.sh
@@ -5,6 +5,8 @@ export JAVA_HOME="${JAVA_HOME}"
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
 status=$?
 
+echo "The status: $status"
+
 # add annotation if failed
 if [ "$status" -ne 0 ]; then
   echo "Hello world"
