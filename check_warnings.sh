bdf1578584131636922b03e40c700ca612ab113b
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-26 20:37:21 -0800

    Avoiding substitution

--- check_warnings.sh
+++ check_warnings.sh
@@ -1,19 +1,24 @@
 echo ""
 echo "Checking Warnings..."
 
+export RESULT="$"
+export RESULT="${RESULT}RESULT"
 export JAVA_HOME="${JAVA_HOME}"
-mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
-result=$?
 
-echo "Result was -->$result<--"
+mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
+RESULT=$?
+
+echo "Result was -->$RESULT<--"
 
 # add annotation if failed
-if [[ -n "$result" ]];
+if [[ -n "$RESULT" ]];
 then
-  if [[ $result -ne 0 ]];
+  echo "EMPTY"
+
+  if [[ $RESULT -ne 0 ]];
   then
     echo "Hello world"
-    exit $result;
+    exit $RESULT;
   fi
 fi
 
