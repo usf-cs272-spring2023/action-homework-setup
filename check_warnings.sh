d6a0215a8c4a85f28afc89e9ac141f5abd8b630e
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-26 19:38:10 -0800

    Updating syntax

--- check_warnings.sh
+++ check_warnings.sh
@@ -3,12 +3,11 @@ echo "Checking Warnings..."
 
 export JAVA_HOME="${JAVA_HOME}"
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
-
-# add annotation if failed
 status=$?
 
+# add annotation if failed
 if [ $status -ne 0 ]; then
-  echo "::warning::Unable to compiling code without warnings. See the \"Check Warnings\" section in the run log for details."
+  echo "Hello world"
 fi
 
 exit $status
