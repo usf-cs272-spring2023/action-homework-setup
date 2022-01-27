fabd37ca0f00bbba7db276de0f2b14cfb4e167b1
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-26 20:41:31 -0800

    Avoiding substitution

--- check_warnings.sh
+++ check_warnings.sh
@@ -1,23 +1,14 @@
 echo ""
 echo "Checking Warnings..."
 
-export DOLLAR="$"
 export JAVA_HOME="${JAVA_HOME}"
 
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
-RESULT=$?
 
-echo "Result was -->${DOLLAR}RESULT<--"
-
-# add annotation if failed
-if [[ -n "${DOLLAR}RESULT" ]];
-then
-  echo "EMPTY"
-
-  if [[ ${DOLLAR}RESULT -ne 0 ]];
+if [[ $? -ne 0 ]];
   then
     echo "Hello world"
-    exit ${DOLLAR}RESULT;
+    exit 1;
   fi
 fi
 
