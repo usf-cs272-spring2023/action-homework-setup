f39e1d25e3a4391db02d8b1fa7eb632675a4e03a
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-26 21:52:05 -0800

    Resetting logging to default

--- check_warnings.sh
+++ check_warnings.sh
@@ -3,7 +3,8 @@ echo "Checking Warnings..."
 
 export JAVA_HOME="${JAVA_HOME}"
 
-mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" "-Dorg.slf4j.simpleLogger.levelInBrackets=false" "-Dorg.slf4j.simpleLogger.defaultLogLevel=info" "-Dorg.slf4j.simpleLogger.warnLevelString=Warning:" clean compile
+# "-Dorg.slf4j.simpleLogger.levelInBrackets=false" "-Dorg.slf4j.simpleLogger.defaultLogLevel=info" "-Dorg.slf4j.simpleLogger.warnLevelString=Warning:"
+mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
 
 if [[ $? -ne 0 ]];
 then
