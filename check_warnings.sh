f4161ffd53b615e8f688db644cfe6d2b2a35c4df
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-26 20:26:04 -0800

    Checking logic

--- check_warnings.sh
+++ check_warnings.sh
@@ -5,11 +5,16 @@ export JAVA_HOME="${JAVA_HOME}"
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
 result=$?
 
+echo "Result was -->$result<--"
+
 # add annotation if failed
-if [[ -n "$result" && "$result" -ne 0 ]]; then
-  echo "Hello world"
-else
-  echo "Result was -->$?<--"
+if [[ -n "$result" ]];
+then
+  if [[ $result -ne 0 ]];
+  then
+    echo "Hello world"
+    exit $result;
+  fi
 fi
 
-exit $result;
+exit 0;
