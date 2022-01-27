dc93bbe4ea1337a15fd66511dabe7bb42263040d
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-26 20:41:52 -0800

    Fixing syntax

--- check_warnings.sh
+++ check_warnings.sh
@@ -6,10 +6,9 @@ export JAVA_HOME="${JAVA_HOME}"
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
 
 if [[ $? -ne 0 ]];
-  then
-    echo "Hello world"
-    exit 1;
-  fi
+then
+  echo "Hello world"
+  exit 1;
 fi
 
 exit 0;
