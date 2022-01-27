4b924caad015cb3a540dde27219fff5e6fed1c5c
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-26 19:47:32 -0800

    Changing variable name

--- check_warnings.sh
+++ check_warnings.sh
@@ -3,13 +3,13 @@ echo "Checking Warnings..."
 
 export JAVA_HOME="${JAVA_HOME}"
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
-status=$?
+result=$?
 
-echo "The status: $status"
+echo "The status: $result"
 
 # add annotation if failed
-if [ "$status" -ne 0 ]; then
+if [ "$result" -ne 0 ]; then
   echo "Hello world"
 fi
 
-exit $status
+exit $result
