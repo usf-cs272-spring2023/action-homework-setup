bdbb5f5aa0aebf6f76956bc6846bb07cb04ec50f
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-27 23:54:12 -0800

    Removing warning annotation for now

--- check_warnings.sh
+++ check_warnings.sh
@@ -4,11 +4,3 @@ echo "Checking Warnings..."
 export JAVA_HOME="${JAVA_HOME}"
 
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
-
-if [[ $? -ne 0 ]];
-then
-  echo "Found compile warnings!"
-  export COMPILE_WARNING="true"
-fi
-
-exit 0;
