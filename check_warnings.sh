47d01521d8ef4ee4e455e0792abda77f0b199f5e
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-27 22:14:01 -0800

    Moving more into setup

--- check_warnings.sh
+++ check_warnings.sh
@@ -5,11 +5,3 @@ export JAVA_HOME="${JAVA_HOME}"
 
 # "-Dorg.slf4j.simpleLogger.levelInBrackets=false" "-Dorg.slf4j.simpleLogger.defaultLogLevel=info" "-Dorg.slf4j.simpleLogger.warnLevelString=Warning:"
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
-
-if [[ $? -ne 0 ]];
-then
-  touch found_compile_warnings
-  exit 1;
-fi
-
-exit 0;
