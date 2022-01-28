21fa26deea3033f73e85d7433fb3ad855346afc6
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-27 22:54:34 -0800

    Testing warnings

--- check_warnings.sh
+++ check_warnings.sh
@@ -1,4 +1,13 @@
 echo ""
 echo "Checking Warnings..."
+
 export JAVA_HOME="${JAVA_HOME}"
+
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
+
+if [[ $? -ne 0 ]];
+then
+  echo "COMPILE_WARNING=1" >> $GITHUB_ENV
+fi
+
+exit 0;
