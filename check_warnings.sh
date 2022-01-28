57861089448b3985837eb718083d13d28453823c
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-27 22:29:31 -0800

    Changing warning check

--- check_warnings.sh
+++ check_warnings.sh
@@ -1,7 +1,4 @@
 echo ""
 echo "Checking Warnings..."
-
 export JAVA_HOME="${JAVA_HOME}"
-
-# "-Dorg.slf4j.simpleLogger.levelInBrackets=false" "-Dorg.slf4j.simpleLogger.defaultLogLevel=info" "-Dorg.slf4j.simpleLogger.warnLevelString=Warning:"
 mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
