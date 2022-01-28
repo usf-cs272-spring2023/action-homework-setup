6728956d4ec14c66f53f562e7b627359baa172fa
Sophie Engle <sjengle@cs.usfca.edu>
2022-01-28 10:59:57 -0800

    Removing annotation processing

--- check_warnings.sh
+++ check_warnings.sh
@@ -3,4 +3,4 @@ echo "Checking Warnings..."
 
 export JAVA_HOME="${JAVA_HOME}"
 
-mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
+mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:all,-processing" "-DcompileOptionXdoclint=-Xdoclint:all/private" "-DcompileOptionFail=true" "-Dmaven.compiler.showWarnings=true" clean compile
