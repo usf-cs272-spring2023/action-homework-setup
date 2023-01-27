67f6948297790f2cb83b239cb9668648a34631a8
Sophie Engle <sjengle@cs.usfca.edu>
2023-01-27 07:59:36 -0800

    Minor updates

--- recompile_homework.sh
+++ recompile_homework.sh
@@ -1,4 +1,4 @@
 echo ""
 echo "Recompiling Homework..."
 export JAVA_HOME="${JAVA_HOME}"
-mvn -f pom.xml -ntp "-DcompileOptionXlint=-Xlint:none" "-DcompileOptionXdoclint=-Xdoclint:none" "-DcompileOptionFail=false" "-Dmaven.compiler.showWarnings=false" clean compile test-compile
+mvn -f pom.xml -ntp --quiet "-DcompileOptionXlint=-Xlint:none" "-DcompileOptionXdoclint=-Xdoclint:none" "-DcompileOptionFail=false" "-Dmaven.compiler.showWarnings=false" clean compile test-compile
